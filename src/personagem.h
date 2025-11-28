4daf36b8f74582cbec1995e340feec6433ddc2a6
Lucas Daniel de Borja <lucasborja125@gmail.com>
2025-11-28 11:14:32 -0300

    Define Personagem struct with additional fields

--- src/personagem.h
+++ src/personagem.h
@@ -1,13 +1,21 @@
 #ifndef SRC_PERSONAGEM_H_
 #define SRC_PERSONAGEM_H_
 
-typedef struct personagem Personagem;
+typedef struct personagem {
+    char nome[50];
+    int nivel;
+    char dado[4];
+    int iniciativa;
+} Personagem;
 
 Personagem* criarPersonagem(char nome[50], int nivel, int idDado);
 void apagarPersonagem(Personagem** p);
+
 int uparNivel(Personagem* p);
+int converteDado(Personagem* p);
 int novaRolagem(Personagem* p);
 int calcularIniciativa(Personagem* p);
+
 void printPersonagem(Personagem* p);
 int alterarDado(Personagem* p, int idDado);
 
